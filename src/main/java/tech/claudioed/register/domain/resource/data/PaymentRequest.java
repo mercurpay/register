4951ef0f3c5f9b1a15edec7b63b52df36449bbf4
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2019-03-05 13:08:20 -0300

    configure ignore unknown

--- src/main/java/tech/claudioed/register/domain/resource/data/PaymentRequest.java
+++ src/main/java/tech/claudioed/register/domain/resource/data/PaymentRequest.java
@@ -1,5 +1,6 @@
 package tech.claudioed.register.domain.resource.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import lombok.Data;
  * Project register
  */
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PaymentRequest {
 
   private BigDecimal value;
