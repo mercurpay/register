10b46ea2395484b24ed31cc48a5f782472aaa5ab
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2019-03-24 11:48:35 -0300

    timer

--- src/main/java/tech/claudioed/register/domain/resource/PaymentResource.java
+++ src/main/java/tech/claudioed/register/domain/resource/PaymentResource.java
@@ -1,9 +1,13 @@
 package tech.claudioed.register.domain.resource;
 
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Timer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
@@ -14,27 +18,36 @@ import tech.claudioed.register.domain.resource.data.PaymentRequest;
 import tech.claudioed.register.domain.service.PaymentService;
 
 /** @author claudioed on 2019-03-01. Project register */
+@Slf4j
 @RestController
 @RequestMapping("/api/payments")
 public class PaymentResource {
 
   private final PaymentService paymentService;
 
-  public PaymentResource(PaymentService paymentService) {
+  private final Timer timer;
+
+  public PaymentResource(PaymentService paymentService,
+      @Qualifier("registerTimer") Timer timer) {
     this.paymentService = paymentService;
+    this.timer = timer;
   }
 
   @PostMapping
   @Timed(value = "register.payment.time.seconds")
-  public ResponseEntity<Payment> newPayment(
+  public ResponseEntity<Payment> newPayment(@RequestHeader(value = "Host",required = false)String host,
       @RequestBody PaymentRequest request, UriComponentsBuilder uriBuilder) {
-    try {
-      final Payment payment = this.paymentService.newPayment(request);
-      final UriComponents uriComponents =
-          uriBuilder.path("api/payments/{id}").buildAndExpand(payment.getId());
-      return ResponseEntity.created(uriComponents.toUri()).body(payment);
-    } catch (PaymentDenied ex) {
-      return ResponseEntity.unprocessableEntity().body(ex.getPayment());
-    }
+    log.info("Request Host {}",host);
+    return timer.record(() -> {
+      try {
+        final Payment payment = this.paymentService.newPayment(request);
+        final UriComponents uriComponents =
+            uriBuilder.path("api/payments/{id}").buildAndExpand(payment.getId());
+        return ResponseEntity.created(uriComponents.toUri()).body(payment);
+      } catch (PaymentDenied ex) {
+        return ResponseEntity.unprocessableEntity().body(ex.getPayment());
+      }
+    });
   }
+
 }
